27c07e3dc42f3803e39dd706749dba45a6674f40
Lostorder <842622982@qq.com>
2026-04-14 17:18:41 +0800

    初始工程

---
+++ src/main/java/cn/wujizone/coconablog/CoconaBlogApplication.java
@@ -0,0 +1,13 @@
+package cn.wujizone.coconablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoconaBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoconaBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/wujizone/coconablog/CoconaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wujizone.coconablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoconaBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
